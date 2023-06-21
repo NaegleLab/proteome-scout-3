3dc2261d174495e3a490bb5807235f44b4f3abbc
saqibrizvi11 <49314153+saqibrizvi11@users.noreply.github.com>
2023-06-21 01:55:44 -0400

    added necessary settings for celery beat

--- app/celery_utils.py
+++ app/celery_utils.py
@@ -1,11 +1,32 @@
+from celery.schedules import crontab
+
 def init_celery(celery, app):
 
     celery.conf.update(app.config)
 
-    # class ContextTask(celery.Task):
-    #     def __call__(self, *args, **kwargs):
-    #         with app.app_context():
-    #             return self.run(*args, **kwargs)
+    celery.conf.update(
+        enable_utc = False,
+        timezone = 'America/New_York'
+    )
 
-    # celery.Task = ContextTask
+    celery.conf.beat_schedule = {"run-me-on_wednesday": {
+        "task": "scripts.export.test_task.test",
+        "schedule": crontab(minute=29, hour=0, day_of_week='wednesday'),
+        "args": ("hello",)
+        }
+    }
+    
+    # celery.conf.beat_schedule = {"run-me-every-ten-seconds": {
+    #     "task": "scripts.export.test_task.test",
+    #     "schedule": 10.0,
+    #     "args": ("hello",)
+    #     }
+    # }
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
     return celery
