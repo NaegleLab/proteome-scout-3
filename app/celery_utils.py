bfcf6aa8787f2e2e60b08d41570b4089b5d7a22a
Logan Campbell <frh7zc@virginia.edu>
2024-05-31 15:16:17 -0400

    adding in email logs celery beat, Not working yet

--- app/celery_utils.py
+++ app/celery_utils.py
@@ -15,7 +15,14 @@ def init_celery(celery, app):
     #    "args": ("hello",)
     #    }
     #}
-
+    celery.conf.beat_schedule = {
+    # other tasks here...
+        'send-log-email-every-day': {
+            'task': 'email.send_email_with_logs',  # replace with the actual name of your task
+            'schedule': crontab(),  # execute daily at midnight
+            'args': ('frh7zc@virginia.edu', 'Daily Log Email', 'Here are the logs for today.'),  # replace with your actual arguments
+        },
+    }
     
     
     # celery.conf.beat_schedule = {"run-me-every-ten-seconds": {
