10020d1ebe763efab0864feb89ab5f5e81ade70d
Logan Campbell <frh7zc@virginia.edu>
2024-06-02 14:47:19 -0400

    added info to celery beat process

--- app/celery_utils.py
+++ app/celery_utils.py
@@ -21,6 +21,7 @@ def init_celery(celery, app):
             'task': 'app.utils.email.send_email_with_logs',  # replace with the actual name of your task
             'schedule': crontab(hour='*/6'),  # execute every 6 hours'schedule'
             'args': ('frh7zc@virginia.edu', 'Daily Log Email', 'Here are the logs for today.'),  # replace with your actual arguments
+            # Set email to whoever is primarily responsible for app
         },
     }
     
