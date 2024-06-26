f4f49daa5ba5ff94e253a38b8fe1fdcb94b80608
Logan Campbell <frh7zc@virginia.edu>
2024-06-26 13:15:24 -0400

    added exception for error when protein doesnt have site_pos

--- app/database/protein.py
+++ app/database/protein.py
@@ -173,7 +173,13 @@ class ProteinRegion(db.Model):
 
 
     def has_site(self, site_pos):
-        return self.start <= site_pos and site_pos <= self.stop
+        if self.start is not None and self.stop is not None:
+            return self.start <= site_pos and site_pos <= self.stop
+        else:
+        # Handle the case where self.start or self.stop is None.
+        # This might involve returning False, or implementing some other logic
+        # that makes sense for your application.
+            return False
 
 class Protein(db.Model):
     __tablename__='protein'
